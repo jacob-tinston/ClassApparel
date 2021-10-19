fe64c7a90a2b9a38ec792cb3a94a778c71912b22
Jacob <developers@surgems.co.uk>
2021-10-19 15:04:32 +0100

    Added database

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
 const express = require('express');
 const app = express();
-const port = 3003;
+const PORT = 3003;
 
-// This displays message that the server running and listening to specified port
-app.listen(port, () => console.log(`Listening on port ${port}`)); //Line 6
+app.use(express.urlencoded({extended: true}));
+app.use(express.json()); // Body Parser
+
+// Connects Database
+const db = require('./src/db/db.js');
+const pool = db.pool;
+const client = db.client;
+const connect = db.connect;
+connect();
 
 // GET route
-app.get('/backend', (req, res) => { //Line 9
+app.get('/test', (req, res) => {
   res.json([
       {id: 1, username: "someone"},
       {id: 2, username: "someone2"}
   ]);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`);
 });
---
+++ src/db/postgres.sql
@@ -0,0 +1,26 @@
+CREATE DATABASE class_apparel;
+
+-- Sequence and defined type
+CREATE SEQUENCE IF NOT EXISTS profiles_id_seq;
+
+-- Table Definition
+CREATE TABLE "public"."profiles" (
+    "id" int4 NOT NULL DEFAULT nextval('profiles_id_seq'::regclass),
+    "forename" varchar(36) NOT NULL,
+    "surname" varchar(36) NOT NULL,
+    "email" varchar(50) NOT NULL,
+    "password" text NOT NULL,
+    PRIMARY KEY ("id")
+);
+
+-- Sequence and defined type
+CREATE SEQUENCE IF NOT EXISTS products_id_seq;
+
+-- Table Definition
+CREATE TABLE "public"."products" (
+    "id" int4 NOT NULL DEFAULT nextval('products_id_seq'::regclass),
+    "title" text NOT NULL,
+    "price" int4,
+    "image_src" text,
+    PRIMARY KEY ("id")
+);
